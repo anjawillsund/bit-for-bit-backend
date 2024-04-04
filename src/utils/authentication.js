27fc49b74f51ee8d51b9d4638958f34acb3f4737
Anja Willsund <aw22hs@student.lnu.se>
2024-04-04 11:48:27 +0200

    Move authentication to utils

--- src/utils/authentication.js
+++ src/utils/authentication.js
@@ -1,32 +1,37 @@
 import jwt from 'jsonwebtoken'
-// TODO: Ska jag använda createError här?
-import createError from 'http-errors'
 
 /**
- * Authenticates a user by verifying the JWT token.
+ * Authenticates token.
  *
  * @param {object} req - Express request object.
  * @param {object} res - Express response object.
  * @param {object} next - Express next middleware function.
  */
-export const authenticateJWT = (req, res, next) => {
-  const authorization = req.headers.authorization?.split(' ')
-  if (authorization?.[0] !== 'Bearer') {
-    next(createError(401))
+export const authenticateToken = (req, res, next) => {
+  // Extract the token from the authorization header
+  const token = req.headers.authorization?.split(' ')[1]
+
+  // Check if the token is missing
+  if (!token) {
+    console.log('No token provided.')
+    res.status(401).json({ error: 'No token provided.' })
     return
   }
-  try {
-    req.jwt = jwt.verify(authorization[1], process.env.PUBLIC_RSA_KEY)
-    req.userId = req.jwt.id
-    req.userRole = req.jwt.role
-    req.username = req.jwt.username
 
-    next()
-  } catch (error) {
-    if (error.name === 'JsonWebTokenError' || error.name === 'TokenExpiredError') {
-      next(createError(401))
-    } else {
-      next(createError(500))
+  // TODO: Är detta rätt hantering av JWT?
+  // Verify the token using the secret key
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    // Check if there is an error during token verification
+    if (err) {
+      console.log(err)
+      // Return a 401 error if the token is invalid
+      res.status(401).json({ error: 'Invalid token.' })
+      return
     }
-  }
+    // Attach the decoded user information to the request object for further processing
+    req.user = decoded
+    console.log('Token authenticated')
+    // Proceed to the next middleware or route handler if the token is valid
+    next()
+  })
 }
