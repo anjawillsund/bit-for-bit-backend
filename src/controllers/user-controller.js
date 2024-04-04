9c17c1cc90d8a6fbbf5c7df20fb406b926458165
Anja Willsund <aw22hs@student.lnu.se>
2024-04-04 11:48:41 +0200

    Move authentication to utils

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -14,41 +14,6 @@ import jwt from 'jsonwebtoken'
  */
 export class UserController {
   /**
-   * Authenticates token.
-   *
-   * @param {object} req - Express request object.
-   * @param {object} res - Express response object.
-   * @param {object} next - Express next middleware function.
-   * @returns {object} - Express response object.
-   */
-  async authenticateToken (req, res, next) {
-    // Extract the token from the authorization header
-    const token = req.headers.authorization?.split(' ')[1]
-
-    // Check if the token is missing
-    if (!token) {
-      console.log('No token provided.')
-      return res.status(401).json({ error: 'No token provided.' })
-    }
-
-    // TODO: Är detta rätt hantering av JWT?
-    // Verify the token using the secret key
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      // Check if there is an error during token verification
-      if (err) {
-        console.log(err)
-        // Return a 401 error if the token is invalid
-        return res.status(401).json({ error: 'Invalid token.' })
-      }
-      // Attach the decoded user information to the request object for further processing
-      req.user = decoded
-      console.log('Token authenticated')
-      // Proceed to the next middleware or route handler if the token is valid
-      next()
-    })
-  }
-
-  /**
    * Tries to log in to user account.
    *
    * @param {object} req - Express request object.
