f4a735b3027e83f37596ca6fbc2d06c0d506cb3f
Anja Willsund <aw22hs@student.lnu.se>
2024-04-04 11:49:20 +0200

    Import authentication from utils

--- src/routes/user-router.js
+++ src/routes/user-router.js
@@ -6,6 +6,7 @@
  */
 
 import express from 'express'
+import { authenticateToken } from '../utils/authentication.js'
 import { UserController } from '../controllers/user-controller.js'
 
 export const router = express.Router()
@@ -16,5 +17,5 @@ router.post('/login', controller.loginPost)
 router.post('/create', controller.createPost)
 // router.get('/get-groups', controller.authenticateToken, controller.getUserGroups)
 // router.post('/add-group', controller.authenticateToken, controller.addGroup)
-router.get('/logout', controller.authenticateToken, controller.logout)
-router.get('/delete-user', controller.authenticateToken, controller.deleteUser, controller.logout)
+router.get('/logout', authenticateToken, controller.logout)
+router.get('/delete-user', authenticateToken, controller.deleteUser, controller.logout)
