1fc2be83c1c140aef7b6f4add0b5d1df442db71e
Anja Willsund <aw22hs@student.lnu.se>
2024-04-10 17:03:28 +0200

    Update JSDoc comment

--- src/utils/authorization.js
+++ src/utils/authorization.js
@@ -1,12 +1,12 @@
 import createError from 'http-errors'
 
 /**
-   * Authorizes a user to edit a specific puzzle for the authenticated user.
-   *
-   * @param {object} req - Express request object.
-   * @param {object} res - Express response object.
-   * @param {object} next - Express next middleware function.
-   */
+ * Authorizes a user to edit a specific puzzle for the authenticated user.
+ *
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @param {object} next - Express next middleware function.
+ */
 export const authorizeUser = (req, res, next) => {
   try {
     if (req.puzzle.owner.toString() !== req.user.id) {
