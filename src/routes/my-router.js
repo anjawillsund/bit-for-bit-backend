6d364c69304ce4a2d9b9bf05199a932360f38517
Anja Willsund <aw22hs@student.lnu.se>
2024-04-10 16:05:32 +0200

    Add id handling and multer

--- src/routes/my-router.js
+++ src/routes/my-router.js
@@ -6,18 +6,33 @@
  */
 
 import express from 'express'
+import multer from 'multer'
 import { authenticateToken } from '../utils/authentication.js'
 import { PuzzleController } from '../controllers/puzzle-controller.js'
 
 export const router = express.Router()
 
 const puzzleController = new PuzzleController()
-
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 10 * 1024 * 1024 }
+})
+router.param('id', (req, res, next, id) => puzzleController.loadPuzzle(req, res, next, id))
 router.route('/puzzles')
   .post(authenticateToken,
+    upload.single('image'),
     (req, res, next) => puzzleController.addPuzzle(req, res, next))
   .get(authenticateToken,
     (req, res, next) => puzzleController.getAllPuzzles(req, res, next))
+
+router.route('/puzzles/:id')
+  .get(authenticateToken,
+    (req, res, next) => puzzleController.getPuzzle(req, res, next))
+  // .put(authenticateToken,
+  //   upload.single('image'),
+  //   (req, res, next) => puzzleController.updatePuzzle(req, res, next))
+  // .delete(authenticateToken,
+  //   (req, res, next) => puzzleController.deletePuzzle(req, res, next))
 // router.post('/create', controller.createPost)
 // router.get('/get-groups', controller.authenticateToken, controller.getUserGroups)
 // router.post('/add-group', controller.authenticateToken, controller.addGroup)
