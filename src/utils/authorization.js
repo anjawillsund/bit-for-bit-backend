11f8d6d58ecb1d85a7dc58e1c7a81cbdbbee42ed
Anja Willsund <aw22hs@student.lnu.se>
2024-04-10 16:56:58 +0200

    Add authorization

---
+++ src/utils/authorization.js
@@ -0,0 +1,20 @@
+import createError from 'http-errors'
+
+/**
+   * Authorizes a user to edit a specific puzzle for the authenticated user.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {object} next - Express next middleware function.
+   */
+export const authorizeUser = (req, res, next) => {
+  try {
+    if (req.puzzle.owner.toString() !== req.user.id) {
+      next(createError(403))
+    } else {
+      next()
+    }
+  } catch (error) {
+    next(error)
+  }
+}
