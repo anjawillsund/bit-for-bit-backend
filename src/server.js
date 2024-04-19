0940afad93b650663f41098cd71e07f05d10f73e
Anja Willsund <aw22hs@student.lnu.se>
2024-04-19 16:42:09 +0200

    Update headers

--- src/server.js
+++ src/server.js
@@ -40,11 +40,13 @@ try {
 
     if (allowedOrigins.includes(origin)) {
       res.setHeader('Access-Control-Allow-Origin', origin)
+      res.setHeader('Access-Control-Allow-Credentials', 'true')
     }
 
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+
     if (req.method === 'OPTIONS') {
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
       return res.sendStatus(200)
     }
 
